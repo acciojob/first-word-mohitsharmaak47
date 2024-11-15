6a64eb748a5d548a82eb4440990a6f7d4c1f6915
mohitsharmaak47 <mohittsharmaak47@gmail.com>
2024-11-15 15:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	 str = str.trim();
+    
+    const spaceIndex = str.indexOf(' ');
+    
+    if (spaceIndex === -1) {
+        return str;
+    }
+    return str.substring(0, spaceIndex);
 }
+
+}
+
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
